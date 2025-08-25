9dca97143b780ec01946870fedc7ac73eaa58b04
Ion Leu <89615621+LeuIonOfficial@users.noreply.github.com>
2025-08-26 00:18:04 +0300

    update public

--- src/components/sections/Event.tsx
+++ src/components/sections/Event.tsx
@@ -31,7 +31,7 @@ export default function Event({ event }: EventProps) {
     date: getWeddingDate(),
     time: getCeremonyTime(),
     address: getWeddingAddress(),
-    image: "/images/event/ceremony.jpg",
+    image: "/images/details/3.png",
     backgroundColor: "#E8C4C4",
   };
 
@@ -39,14 +39,10 @@ export default function Event({ event }: EventProps) {
     date: getWeddingDate(),
     time: getReceptionTime(),
     address: getWeddingAddress(),
-    image: "/images/event/reception.jpg",
+    image: "/images/details/5.png",
     backgroundColor: "#D4C4A8",
   };
 
-  const mapUrl =
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423286.27405770525!2d-118.69192047471653!3d34.02016130653294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sus!4v1622839304757!5m2!1sen!2sus";
-  const contactEmail = "johnandjanewedding@example.com";
-  // Animation refs
   const [titleRef, titleInView] = useInView({
     triggerOnce: true,
     threshold: 0.1,
